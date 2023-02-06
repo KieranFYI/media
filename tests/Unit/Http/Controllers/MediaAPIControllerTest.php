ec3770471a6215661ca35044944664d862b774ac
Kieran Anderson <kieran@andersons.bz>
2023-02-06 21:35:42 +0000

    More tests + fixes

--- tests/Unit/Http/Controllers/MediaAPIControllerTest.php
+++ tests/Unit/Http/Controllers/MediaAPIControllerTest.php
@@ -2,28 +2,100 @@
 
 namespace KieranFYI\Tests\Media\Unit\Http\Controllers;
 
-use Illuminate\Http\JsonResponse;
-use KieranFYI\Media\Http\Controllers\MediaAPIController;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Auth\Middleware\Authorize;
+use Illuminate\Http\UploadedFile;
+use KieranFYI\Media\Core\Facades\MediaStorage;
+use KieranFYI\Roles\Core\Http\Middleware\HasPermission;
 use KieranFYI\Tests\Media\TestCase;
+use Orchestra\Testbench\Http\Middleware\VerifyCsrfToken;
 
 class MediaAPIControllerTest extends TestCase
 {
 
-    /**
-     * @var MediaAPIController
-     */
-    private MediaAPIController $controller;
-
     protected function setUp(): void
     {
         parent::setUp();
-        $this->controller = new MediaAPIController();
     }
 
     public function testIndex()
     {
         $this->artisan('migrate');
-        $response = $this->controller->index();
-        $this->assertInstanceOf(JsonResponse::class, $response);
+        $response = $this->withoutMiddleware()
+            ->get(route('admin.api.media.index'))
+            ->json();
+        $this->assertIsArray($response);
+    }
+
+    public function testStore()
+    {
+        $this->artisan('migrate');
+        $response = $this->withoutMiddleware()
+            ->post(route('admin.api.media.store'), [
+                'files' => [
+                    UploadedFile::fake()->image('test.png')
+                ]
+            ])
+            ->json();
+        $this->assertIsArray($response);
+        $this->assertEquals('test.png', $response[0]['file_name']);
+    }
+
+    public function testShow()
+    {
+        $this->artisan('migrate');
+        $media = MediaStorage::store(UploadedFile::fake()->image('test.png'));
+        $response = $this
+            ->withoutMiddleware([
+                HasPermission::class,
+                Authenticate::class,
+                VerifyCsrfToken::class,
+                \Orchestra\Testbench\Http\Middleware\Authenticate::class,
+                Authorize::class,
+            ])
+            ->get(route('admin.api.media.show', $media))
+            ->json();
+        $this->assertEquals($media->toArray(), $response);
+    }
+
+    public function testDestroy()
+    {
+        $this->artisan('migrate');
+        $media = MediaStorage::store(UploadedFile::fake()->image('test.png'));
+        $response = $this
+            ->withoutMiddleware([
+                HasPermission::class,
+                Authenticate::class,
+                VerifyCsrfToken::class,
+                \Orchestra\Testbench\Http\Middleware\Authenticate::class,
+                Authorize::class,
+            ])
+            ->delete(route('admin.api.media.destroy', $media))
+            ->json();
+        $this->assertIsArray($response);
+        $this->assertEmpty($response);
+        $this->assertTrue($media->refresh()->trashed());
+    }
+
+    public function testSearch()
+    {
+        $this->artisan('migrate');
+        $media = MediaStorage::store(UploadedFile::fake()->image('test.png'));
+        $response = $this
+            ->withoutMiddleware([
+                HasPermission::class,
+                Authenticate::class,
+                VerifyCsrfToken::class,
+                \Orchestra\Testbench\Http\Middleware\Authenticate::class,
+                Authorize::class,
+            ])
+            ->post(route('admin.api.media.search', $media), [
+                'search' => 'test.png'
+            ])
+            ->json();
+        $this->assertIsArray($response);
+        $this->assertNotEmpty($response['data']);
+        $this->assertCount(1, $response['data']);
+        $this->assertEquals('test.png', $response['data'][0]['file_name']);
     }
 }
--- tests/Unit/Http/Controllers/MediaControllerTest.php
+++ tests/Unit/Http/Controllers/MediaControllerTest.php
@@ -3,11 +3,8 @@
 namespace KieranFYI\Tests\Media\Unit\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\UploadedFile;
-use KieranFYI\Media\Core\Facades\MediaStorage;
 use KieranFYI\Media\Http\Controllers\MediaController;
 use KieranFYI\Tests\Media\TestCase;
-use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class MediaControllerTest extends TestCase
 {
@@ -31,15 +28,4 @@ class MediaControllerTest extends TestCase
         $this->assertIsString($response->render());
         $this->assertStringContainsString('<media-index />', $response);
     }
-
-
-
-    public function testShow()
-    {
-        $this->artisan('migrate');
-        $media = MediaStorage::store(UploadedFile::fake()->image('test.png'));
-        $response = $this->controller->show($media->versions->first(), 'png');
-        $this->assertInstanceOf(StreamedResponse::class, $response);
-    }
-
 }
